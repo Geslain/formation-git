d1da498ce904dd2eab4e493e8b9580120d2ffe62
Fabien Roux <fabien.roux@keyrus.com>
2018-03-19 16:32:19 +0100

    Form V3

--- php/index.php
+++ php/index.php
@@ -8,16 +8,16 @@ include 'operator.php';
 <body>
 	<p><?=$error?></p>
 	<form action="index.php">
-		<input type="reset">C</input>
+		<input type="reset" value="C"></input>
 		<input type="text" id="a" name="a"></input>
-		<select>
+		<select id="op" name="op">
 			<option value="+">+</option>
 			<option value="-">-</option>
 			<option value="*">*</option>
 			<option value="/">/</option>
 		</select>
 		<input type="text" id="b" name="b"></input>
-		<input type="submit">=</input>
+		<input type="submit" value="="></input>
 	</form>
 	<p><?=$result?></p>
 </body>
