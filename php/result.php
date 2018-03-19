90785e793c41ea5b0f38991bd097bfb87f0a2359
Oussama <oussama.noubair@gmail.com>
2018-03-19 15:26:26 +0100

    ceci est la template

---
+++ php/result.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Answer</title>
+</head>
+<body>
+<p>The answer is: 
+<?php
+
+$valeur1 = $_POST['first'] ; // c'est la valeur du textbox1
+$valeur2 = $_POST['second'] ; // c'est la valeur du textbox2
+$indicator = $_POST['group1']; // c'est la valeur de l'opération utilisé
+
+//Fonction addition
+
+
+//Fonction soustraction
+
+?>
+</p> 
+</body>
+</html>
