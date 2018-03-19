0ec59c0b4efadef1aeb8f27061d88cbbd4f2b5bb
Thanh-Tuan Mai <Thanh-Tuan.Mai@keyruscorp.key>
2018-03-19 16:23:46 +0100

    Ajout de la calculatrice avec Oussama depuis php_addition

---
+++ php/result.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Answer</title>
+</head>
+<body>
+<p>The answer is: 
+<?php
+
+$valeur1 = $_POST['first'] ; // c'est la valeur du textbox1
+$valeur2 = $_POST['second'] ; // c'est la valeur du textbox2
+$indicator = $_POST['group1']; // c'est la valeur de l'opération utilisé
+
+//Fonction addition
+if($indicator == 'ajout') {
+	$result = $valeur1 + $valeur2 ;
+echo "la valeur de la somme : $result";
+}
+
+//Fonction soustraction
+
+if($indicator == 'soustraction') {
+	$result = $valeur1 - $valeur2 ;
+echo "la valeur de la soustraction : $result";
+}
+
+
+//Fonction division
+
+if($indicator == 'division') {
+	$result = $valeur1 / $valeur2 ;
+echo "la valeur de la division : $result";
+}
+//Fonction multiplication
+
+if($indicator == 'multi') {
+	$result = $valeur1 * $valeur2 ;
+echo "la valeur de la multiplication : $result";
+
+}
+
+//Fonction Modulo
+
+if($indicator == 'modulo') {
+	$result = $valeur1 % $valeur2 ;
+echo "la valeur du modulo : $result";
+
+}
+
+?>
+</p> 
+</body>
+</html>
