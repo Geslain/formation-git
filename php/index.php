d56016faab16ac6a3b6b01ca626423491a8ac1e5
Fabien Roux <fabien.roux@keyrus.com>
2018-03-19 16:08:00 +0100

    Form created

---
+++ php/index.php
@@ -0,0 +1,17 @@
+<html>
+<head></head>
+<body>
+	<form action="">
+		<input type="text" id="a" name="a"></input>
+		<select>
+			<option value="+">+</option>
+			<option value="-">-</option>
+			<option value="*">*</option>
+			<option value="/">/</option>
+		</select>
+		<input type="text" id="b" name="b"></input>
+		<input type="reset">C</input>
+		<input type="submit">=</input>
+	</form>
+</body>
+</html>
