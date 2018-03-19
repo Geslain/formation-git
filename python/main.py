432535df8cce74a6810295050865fe153fff8017
Olivier_keyrus <olivier.guilmin@keyrus.com>
2018-03-19 16:39:14 +0100

    creation du main

---
+++ python/main.py
@@ -0,0 +1,13 @@
+import mult.py
+import addition.py
+import division.py
+import soustraction.py
+
+if operation == 'multiplication':
+    multiplication(a, b)
+elif operation == 'addition':
+    addition(a, b)
+elif operation == 'soustraction':
+	soustraction(a, b)
+elif operation == 'division':
+    division(a, b)
